4c9b8233fe5521ba6e504ad91cee871e4f907f82
daniyal <daniyalirfan.itdev@gmail.com>
2022-01-23 19:44:48 +0400

    libs updated and support for 12 version android

--- app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/activity/MainViewModel.kt
+++ app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/activity/MainViewModel.kt
@@ -4,8 +4,11 @@ import androidx.hilt.lifecycle.ViewModelInject
 import com.daniyalirfan.kotlinbasewithcorutine.baseclasses.BaseViewModel
 import com.daniyalirfan.kotlinbasewithcorutine.data.remote.reporitory.MainRepository
 import com.daniyalirfan.kotlinbasewithcorutine.utils.NetworkHelper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel @ViewModelInject constructor(
+@HiltViewModel
+class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
     private val networkHelper: NetworkHelper
 ) : BaseViewModel() {
