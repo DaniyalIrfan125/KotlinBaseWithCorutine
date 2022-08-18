d63ea2126ab5fdc186e609cec435449daccbb10e
daniyal irfan <daniyal@vp.ae>
2022-08-18 11:43:07 +0400

    libraries updated

--- app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/activity/MainViewModel.kt
+++ app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/activity/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.daniyalirfan.kotlinbasewithcorutine.ui.activity
 
-import androidx.hilt.lifecycle.ViewModelInject
 import com.daniyalirfan.kotlinbasewithcorutine.baseclasses.BaseViewModel
 import com.daniyalirfan.kotlinbasewithcorutine.data.remote.reporitory.MainRepository
 import com.daniyalirfan.kotlinbasewithcorutine.utils.NetworkHelper
@@ -11,7 +10,4 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
     private val networkHelper: NetworkHelper
-) : BaseViewModel() {
-
-
-}
+) : BaseViewModel()
--- app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/firstfragment/FirstViewModel.kt
+++ app/src/main/java/com/daniyalirfan/kotlinbasewithcorutine/ui/firstfragment/FirstViewModel.kt
@@ -1,7 +1,6 @@
 package com.daniyalirfan.kotlinbasewithcorutine.ui.firstfragment
 
 import androidx.databinding.ObservableField
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
